500ec7c4e58430ef5d19288e922f55eea9fb5064
Elanny Torres <torres.elanny@gmail.com>
2022-05-04 22:39:33 -0500

    lazy load for auth and heroes

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -7,6 +7,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 
+import { HeroesRoutingModule } from './heroes-routing.module';
+
 
 
 @NgModule({
@@ -18,7 +20,8 @@ import { ListadoComponent } from './pages/listado/listado.component';
     ListadoComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HeroesRoutingModule
   ]
 })
 export class HeroesModule { }
